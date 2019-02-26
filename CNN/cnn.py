2b2e9373270a6a8bc5335e4e22616dbb66a11840
dingfan <zhaodingfanhaha@gmail.com>
2019-02-26 16:46:26 +0800

    Update CNN model architecture

--- CNN/cnn.py
+++ CNN/cnn.py
@@ -1,18 +1,18 @@
-# This program is used to build the basic model structure
-# This program refers to some ideas from 
-# https://machinelearningmastery.com/multi-class-classification-tutorial-keras-deep-learning-library/
-# Data is fetched from: http://archive.ics.uci.edu/ml/machine-learning-databases/iris/
-
-import numpy as np 
+'''
+Single label multi-class classification 
+'''
 import pandas
-
-# import os; 
-# os.environ['KERAS_BACKEND'] = 'theano'
 
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
 from keras.models import load_model
 from keras.utils import np_utils
+from keras.optimizers import Adam
+from keras.layers.core import Activation
+from keras.layers.core import Dropout
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
@@ -20,58 +20,30 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import confusion_matrix
 from sklearn import cross_validation
 
-# Suppose the input is stored in csv format just like the online dataset 
+# same process for all the programs to read the input 
 dataframe = pandas.read_csv('input/iris.csv', header=0)
 dataset = dataframe.values
-len_data = len(dataset[0])
-properties = dataset[:, 0:len_data-1].astype(float) # property
-labels = dataset[:, len_data-1] # label
+print(dataset)
 
-feature_train, feature_test, labels_train, labels_test = cross_validation.train_test_split(properties, labels, test_size=0.2, random_state=4)
-
-# one-hot encoding for labels 
-# encoder = LabelEncoder()
-# encoder.fit(labels)
-# encoder_label = encoder.transform(labels)
-
-# 0 ==> [1,0,0] 1 ==> [0,1,0] 2==> [0,0,1]
-# one_hot_label = np_utils.to_categorical(encoder_label)
-
-# Use simply fully connected feedforward neural network
-def fully_connected_model():
-    # create model
+def convolutional_model(width, height, depth, finalAct='softmax'): 
+    # final act use softmax as the activation in the end of the neural network structure
     model = Sequential()
-    # build layers 
-    model.add(Dense(8, input_dim=len_data-1, activation='relu'))
-    model.add(Dense(3, activation='softmax')) # use softmax to represent predicted probability
-    # compile model
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    inputShape = (height, width, depth)
+    dimension = -1
+
+    # Conv => Relu => Max Pooling layer
+    model.add(Conv2D(64, (3,3), padding='same', input_shape=inputShape)) 
+    # the number of neurons depends on the size of the input dataset 
+    model.add(Activation("relu"))
+    model.add(Conv2D(32, (3,3), activation='relu'))
     
-    return model
+    model.add(BatchNormalization(axis=dimension))
+    model.add(MaxPooling2D(pool_size=(3,3)))
+    model.add(Dropout(0.2)) # drop out 1/5 of the connections to avoid overfitting 
 
-estimator = KerasClassifier(build_fn=fully_connected_model, epochs=200, batch_size=5, verbose=0)
+    # Add two more set of layers from above architecture
 
-# Confusion matrix as the evaluation method 
-estimator.fit(properties, labels)
-labels_pred = estimator.predict(properties)
-result = confusion_matrix(labels, labels_pred)
-model = fully_connected_model()
-model.save("result/model.h5")
-print('Model saved to disk. ')
-
-load_model('result/model.h5')
-
-print(result) 
-# the testing result :
-# [[49  0  0]
-#  [ 0 46  4]
-#  [ 0  1 49]]
-
-# Another evaluation method ==> accuracy score
-# seed = 11
-# np.random.seed(seed)
-
-# kfold = KFold(n_splits=10, shuffle=True, random_state=seed) 
-# results = cross_val_score(estimator, properties, one_hot_label, cv=kfold)
-
-# print("Accuracy: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
+height = 28
+width = 28
+depth = 1
+model = convolutional_model(width, height, depth)
