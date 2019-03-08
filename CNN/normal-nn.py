4bd502b3b0fa0a09bb99b01357b56c5edb484be4
dingfan <zhaodingfanhaha@gmail.com>
2019-03-08 12:39:25 +0800

    Fix the bug of predicting different results after saving and loading

--- CNN/normal-nn.py
+++ CNN/normal-nn.py
@@ -19,8 +19,10 @@ import pandas
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.models import load_model
+from keras.models import model_from_json
 from keras.utils import np_utils
 from keras.wrappers.scikit_learn import KerasClassifier
+from keras.backend import manual_variable_initialization
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import LabelEncoder
@@ -32,6 +34,7 @@ from sklearn import datasets
 dataframe = pandas.read_csv('input/iris.csv', header=0)
 dataset = dataframe.values
 label_names = datasets.load_iris().target_names
+
 len_data = len(dataset[0])
 feature = dataset[:, 0:len_data-1].astype(float) # property
 label = dataset[:, len_data-1] # label
@@ -58,16 +61,25 @@ def fully_connected_model():
     # build layers 
     model.add(Dense(8, input_dim=len_data-1, activation='relu'))
     model.add(Dense(3, activation='softmax')) # use softmax to represent predicted probability
+
+    # If the effect is not so good, add more hidden layers to increase the accuracy level
     # compile model
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     
     return model
 
-estimator = KerasClassifier(build_fn=fully_connected_model, epochs=200, batch_size=5, verbose=0)
+estimator = KerasClassifier(build_fn=fully_connected_model, epochs=200, batch_size=119, verbose=0)
 
 # Confusion matrix as the evaluation method 
+model = fully_connected_model()
+print(feature_train.shape)
+# model.fit(feature_train, label_train, batch_size=119, epochs=200, verbose=0)
+# label_pred = model.predict_classes(feature_test)
+# print(label_pred)
 estimator.fit(feature_train, label_train)
 label_pred = estimator.predict(feature_test)
+print(label_test)
+print(label_pred)
 
 # Use K-Fold validation
 # Another evaluation method ==> accuracy score
@@ -79,7 +91,15 @@ results = cross_val_score(estimator, feature, one_hot_label, cv=kfold)
 
 print("Accuracy: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
 
+# model.fit(feature_train, label_train, epochs=200, batch_size=5, verbose=0)
+model = estimator.model
+model_label_pred = model.predict_classes(feature_test)
+print(label_test)
+print(model_label_pred)
+
+
 result = confusion_matrix(label_test, label_pred)
+print(result)
 # the testing result :
 # [[17  0  0]
 #  [ 0  5  1]
@@ -92,19 +112,34 @@ one_line_result = confusion_matrix(label_test[0:1], one_line_label_pred)
 print(one_line_result)
 
 model = fully_connected_model()
-print(model)
+weights = model.get_weights()
+print(weights)
 
-model.save("model/normal_nn.h5")
+manual_variable_initialization(True)
+
+# model.save("model/normal_nn.h5")
+model_json = model.to_json()
+with open('model/nn_structure.json', 'w') as f:
+    f.write(model_json)
+model.save_weights("model/weights.h5")
 print('Model saved to disk. ')
 
 # For actual classification scenario, the model will be loaded and used directly 
-loaded_model = load_model('model/normal_nn.h5')
+# loaded_model = load_model('model/normal_nn.h5')
+json_file = open("model/nn_structure.json", 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+loaded_model.load_weights("model/weights.h5")
+print(loaded_model.get_weights())
 loaded_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 loaded_labels_pred = loaded_model.predict_classes(feature_test)
 # print(label_test)
 # print(loaded_labels_pred)
 # for label in loaded_labels_pred:
 #     print(label_names[label])
+
+print(loaded_model.get_weights())
 
 
 
